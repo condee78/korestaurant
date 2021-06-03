82bdc08e31cbfdc7f95c67c8fba61b6fa97b8b88
condee78 <condee78@gmail.com>
2021-06-03 23:56:28 +0700

    using sw and other helpers

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,9 @@ import "../styles/main.css";
 import "../styles/main-responsive.css";
 
 import App from "./views/app";
+import swRegister from "./utils/sw-register";
+import WebSocketInitiator from "./utils/websocket-initiator";
+import CONFIG from "./globals/config";
 
 const app = new App({
   drawer: document.querySelector(".sidenav"),
@@ -16,4 +19,6 @@ window.addEventListener("hashchange", () => {
 
 window.addEventListener("load", () => {
   app.renderPage();
+  swRegister();
+  // WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
