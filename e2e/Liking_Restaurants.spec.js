6cfb5a03d745e4d45cd3b2bafbc6bfcd8d8cbc4f
condee78 <condee78@gmail.com>
2021-07-10 00:19:20 +0700

    e2e liking restaurant

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,5 +1,34 @@
-Feature('Liking Restaurants');
+/* eslint-disable */
+const assert = require("assert");
 
-Scenario('test something', ({ I }) => {
+Feature("Liking Restaurants");
 
+Before((I) => {
+  I.amOnPage("/#/favorite");
+});
+
+Scenario("showing empty liked restaurants", (I) => {
+  I.see("Kamu belum punya restaurant favorite", ".content-items");
+});
+
+Scenario("liking one restaurant", async (I) => {
+  I.see("Kamu belum punya restaurant favorite", ".content-items");
+
+  I.amOnPage("/");
+
+  I.seeElement(".resto-item-title a");
+
+  const firstResto = locate(".resto-item-title a").first();
+  const firstRestoTitle = await I.grabTextFrom(firstResto);
+  I.click(firstRestoTitle);
+
+  I.seeElement("#likeButton");
+  I.click("#likeButton");
+
+  I.amOnPage("/#/favorite");
+  I.seeElement(".resto-item");
+
+  const likedRestoTitle = await I.grabTextFrom(".resto-item-title a");
+
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
 });
